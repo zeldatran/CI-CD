cef86cd2719de2def7027863ee09a635af609c64
zeldatran <146747778+zeldatran@users.noreply.github.com>
2026-06-10 14:13:26 +0700

    test7

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -19,6 +19,15 @@ public class LoginTest extends BaseTest {
         Assert.assertTrue(loginPage.isLoginSuccessful(), "Đăng nhập đúng mật khẩu thất bại");
     }
 
+    @Test(description = "Login with the same valid account again")
+    public void loginWithCorrectPasswordAgain() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        loginPage.login(username, password);
+
+        Assert.assertFalse(loginPage.isLoginFailed(), "Lần đăng nhập đúng nhưng hệ thống báo lỗi");
+    }
+
     @Test(description = "Login with wrong password")
     public void loginWithWrongPassword() {
         LoginPage loginPage = new LoginPage(driver);
@@ -26,5 +35,14 @@ public class LoginTest extends BaseTest {
         loginPage.login(username, "SaiMatKhau123");
 
         Assert.assertTrue(loginPage.isLoginFailed(), "Đăng nhập sai mật khẩu nhưng hệ thống không báo lỗi");
+    }
+
+    @Test(description = "Login with wrong username")
+    public void loginWithWrongUsername() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        loginPage.login("invaliduser@example.com", password);
+
+        Assert.assertTrue(loginPage.isLoginFailed(), "Đăng nhập sai tài khoản nhưng hệ thống không báo lỗi");
     }
 }
